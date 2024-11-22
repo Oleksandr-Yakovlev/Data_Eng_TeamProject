ca207fc93ff819bb92aaf26a4502555db1e1fc72
tahtinen <123938410+tahtinen@users.noreply.github.com>
2024-11-22 12:38:24 +0200

    Update transform_module.py

--- transform_module.py
+++ transform_module.py
@@ -1,16 +1,14 @@
-#Transform Module
+# Transform Module
 import pandas as pd
 import numpy as np
+
+
 def transform_data(**kwargs):
     # Retrieve file path from XCom
     file_path = kwargs['ti'].xcom_pull(key='csv_file_path')
     df = pd.read_csv(file_path)
 
-    # Set id
-    if 'id' not in df.columns:
-        df['id'] = df.index
-
-        # Formatted date column to date format
+    # Formatted date column to date format
     df['Formatted Date'] = pd.to_datetime(df['Formatted Date'], utc=True)
 
     # Handle missing values in critical columns
@@ -24,29 +22,9 @@ def transform_data(**kwargs):
     df['Date'] = df['Formatted Date'].dt.date
     df['Month'] = df['Formatted Date'].dt.month
 
-    # Calculating daily averages
-    daily_avg_temperature = df.groupby('Date')['Temperature (C)'].mean()
-    daily_avg_humidity = df.groupby('Date')['Humidity'].mean()
-    daily_avg_wind_speed = df.groupby('Date')['Wind Speed (km/h)'].mean()
-
-    df['Avg Temperature (C)'] = df['Date'].map(daily_avg_temperature)
-    df['Avg Humidity'] = df['Date'].map(daily_avg_humidity)
-    df['Avg Wind Speed (km/h)'] = df['Date'].map(daily_avg_wind_speed)
-
-    # Define a function to calculate monthly mode value
-    def calculate_monthly_mode(series):
-        mode = series.mode()
-        return mode[0] if len(mode) == 1 else np.nan
-
-    # Use 'calculate_monthly_mode' function
-    monthly_precip_type = df.groupby('Month')['Precip Type'].agg(calculate_monthly_mode)
-
-    # Mapping Mode Values to the DataFrame
-    df['Mode'] = df['Month'].map(monthly_precip_type)
-
     # Define function to categorize wind speed
     def categorize_wind_speed(speed_kmh):
-        speed = speed_kmh / 3.6
+        speed = round(speed_kmh / 3.6, 1)
         if 0 <= speed <= 1.5:
             return 'Calm'
         elif 1.6 <= speed <= 3.3:
@@ -72,8 +50,41 @@ def transform_data(**kwargs):
         elif speed >= 32.7:
             return 'Violent Storm'
 
-    # Using 'categorize_wind_speed' function and write the value to new Wind Strength column.
-    df['Wind Strength'] = df['Wind Speed (km/h)'].apply(categorize_wind_speed)
+    # Create daily weather table grouped by days with mean values
+    daily_weather = df.groupby('Date').agg({
+        'Temperature (C)': 'mean',
+        'Apparent Temperature (C)': 'mean',
+        'Humidity': 'mean',
+        'Wind Speed (km/h)': 'mean',
+        'Visibility (km)': 'mean',
+        'Pressure (millibars)': 'mean',
+    }).reset_index()
+
+    # Rename columns
+    daily_weather.rename(columns={
+        'Date': 'formatted_date',
+        'Temperature (C)': 'avg_temperature_c',
+        'Apparent Temperature (C)': 'avg_apparent_temperature_c',
+        'Humidity': 'avg_humidity',
+        'Wind Speed (km/h)': 'avg_wind_speed_kmh',
+        'Visibility (km)': 'avg_visibility_km',
+        'Pressure (millibars)': 'avg_pressure_millibars',
+
+    }, inplace=True)
+
+    # Create new column 'wind_strength' and us 'categorize_wind_speed 'function to categorizing wind speed
+    daily_weather['wind_strength'] = daily_weather['avg_wind_speed_kmh'].apply(categorize_wind_speed)
+
+    # Create id column put it first position with values corresponding to the row index
+    daily_weather.insert(loc=0, column='id', value=daily_weather.index)
+
+    # Define a function to calculate monthly mode value
+    def calculate_monthly_mode(series):
+        mode = series.mode()
+        return mode[0] if len(mode) == 1 else np.nan
+
+    # Use 'calculate_monthly_mode' function
+    monthly_precip_type = df.groupby('Month')['Precip Type'].agg(calculate_monthly_mode)
 
     # Calculating Monthly averages
     monthly_avg_temperature = df.groupby('Month')['Temperature (C)'].mean()
@@ -82,29 +93,28 @@ def transform_data(**kwargs):
     monthly_avg_visibility = df.groupby('Month')['Visibility (km)'].mean()
     monthly_avg_pressure = df.groupby('Month')['Pressure (millibars)'].mean()
 
-    daily_weather = df[['id', 'Formatted Date', 'Temperature (C)', 'Apparent Temperature (C)', 'Humidity',
-                        'Wind Speed (km/h)', 'Visibility (km)', 'Pressure (millibars)', 'Wind Strength',
-                        'Avg Temperature (C)', 'Avg Humidity', 'Avg Wind Speed (km/h)']]
-
+    # Creating monthly_weather dataframe and apply monthly averages values to it
     monthly_weather = pd.DataFrame({
-        'id': range(1, len(monthly_avg_temperature) + 1),
-        'Month': monthly_avg_temperature.index,
-        'Avg Temperature (C)': monthly_avg_temperature.values,
-        'Avg Apparent Temperature (C)': monthly_avg_apparent_temperature.values,
-        'Avg Humidity': monthly_avg_humidity.values,
-        'Avg Visibility (km)': monthly_avg_visibility.values,
-        'Avg Pressure (millibars)': monthly_avg_pressure.values,
-        'Mode Precip Type': monthly_precip_type.values
+        'month': monthly_avg_temperature.index,
+        'avg_temperature_c': monthly_avg_temperature.values,
+        'avg_apparent_temperature_c': monthly_avg_apparent_temperature.values,
+        'avg_humidity': monthly_avg_humidity.values,
+        'avg_visibility_km': monthly_avg_visibility.values,
+        'avg_pressure_millibars': monthly_avg_pressure.values,
+        'mode_precip_type': monthly_precip_type.values
     })
 
+    # Create id column put it first position with values corresponding to the row index
+    monthly_weather.insert(loc=0, column='id', value=monthly_weather.index)
 
-    daily_weather.to_csv('daily_weather.csv', index=False)
-    monthly_weather.to_csv('monthly_weather.csv', index=False)
+    # Set files paths
+    daily_weather_file_path = '/tmp/daily_weather.csv'
+    monthly_weather_file_path = '/tmp/monthly_weather.csv'
 
-    daily_weather_file_path = 'tmp/daily_weather.csv'
-    monthly_weather_file_path = 'tmp/monthly_weather.csv'
+    # Save files in csv format without indexes
+    daily_weather.to_csv(daily_weather_file_path, index=False)
+    monthly_weather.to_csv(monthly_weather_file_path, index=False)
 
+    # Pass files paths to xcom
     kwargs['ti'].xcom_push(key='daily_weather_csv_file_path', value=daily_weather_file_path)
     kwargs['ti'].xcom_push(key='monthly_weather_csv_file_path', value=monthly_weather_file_path)
-
-
